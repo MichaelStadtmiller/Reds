fbe5e1e3613c0fda941c61419703f10e9463a9df
Michael Stadtmiller <michaelstadtmiller@gmail.com>
2016-04-15 00:46:51 -0400

    progress bars complete!

--- stats/models.py
+++ stats/models.py
@@ -8,6 +8,16 @@ class Player(models.Model):
     Name = models.CharField(max_length=20, primary_key=True)
     Guess = models.IntegerField(default=0)
 
+    def total_games(self):
+        return 162
+
+    def perc_guess(self):
+        return self.Guess/self.total_games()*100
+
+    def Score(self):
+        #return self.perc_guess()-RedsData.wins_proj(RedsData)
+        return 1
+
     def __getitem__(self, item):
         return getattr(self, item)
 
@@ -27,18 +37,22 @@ class RedsData(models.Model):
         return self.won+self.lost
 
     def games_left(self):
-        #total_games = 162
         return self.total_games() - self.games_played()
-        #return total_games - self.games_played()
 
     def perc_won(self):
-        return int(round(self.won/self.total_games()*100))
+        return self.won/self.total_games()*100
 
     def perc_lost(self):
-        return int(round(self.lost/self.total_games()*100))
+        return self.lost/self.total_games()*100
 
     def perc_left(self):
-        return int(round(self.games_left()/self.total_games()*100))
+        return self.games_left()/self.total_games()*100
+
+    def currwinrate(self):
+        return int(round(self.won/self.games_played()*100))
+
+    def wins_proj(self):
+        return int(self.total_games()*(self.currwinrate()/100))
 
     def __str__(self):
         return str(self.lastGamePlayed) + " (" + str(self.won) + "-" + str(self.lost) + ")"
