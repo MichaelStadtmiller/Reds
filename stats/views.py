b847e67c631316fa8d15a4dd0d5faf78fafe429f
Michael Stadtmiller <michaelstadtmiller@gmail.com>
2016-08-09 00:12:16 -0400

    high lows found. note displaying

--- stats/views.py
+++ stats/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 from .models import Player, RedsData
-
+from django.db.models import Func, F
 
 class MainView(TemplateView):
     template_name = 'stats/index.html'
@@ -18,6 +18,23 @@ class MainView(TemplateView):
         data = ([d.wins_proj() for d in reds_data])
         for p in all_players:
             p.score = abs(int(data[0])-p.guess)
+            # get next highest players guess (excluding their own)
+            high = Player.objects.filter(guess__gte=p.guess).exclude(name=p.name).order_by('guess').values('guess').first()
+            if high == None:    # if top, they are the highest
+                high = p.guess
+            else:
+                high = high.values()[0]
+            #TOP END: wins + games left < guess minue half way to next lowest
+            #                    a. query to get next lowest number
+            #                    b. average guess and next lowest
+            #                    c. (wins + left) < avg; stamp it
+            p.high = high
+            # get next lowest players guess (excluding their own)
+            low = Player.objects.filter(guess__lte=p.guess).exclude(name=p.name).order_by('-guess').values('guess').first()
+            if low == None: # if bottom, they are the lowest
+                p.low = p.guess
+            else:
+                p.low = low.values()[0]
         context['players'] = all_players
         return context
 
